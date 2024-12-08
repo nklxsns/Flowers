61ad4e417d03b1d0ad1ba628aecd51c94f9d7e26
nklxsns <nikhil.kamble.0213@gmail.com>
2024-12-08 23:46:13 +0530

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/quiz" element={<Quiz />} />
-          <Route path="/flowers" element={<Flowers />} />
+          <Route path="/flowersPage" element={<Flowers />} />
         </Routes>
       </BrowserRouter>
     </>
